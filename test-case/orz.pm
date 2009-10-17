2603d9d8818fb0210224077ac632338eec2545da
c9s <cornelius.howl@gmail.com>
2009-10-18 04:54:01 +0800

    add test-case

---
+++ test-case/orz.pm
@@ -0,0 +1,25 @@
+
+
+use base qw(Jifty);
+
+sub test1 { 
+}
+
+
+sub test2 {
+
+}
+
+sub current_user {
+
+}
+
+
+sub fail {
+    my $self = shift;
+    $self->
+    $self->cur
+    Jifty->
+    LWP::UserAgent->
+
+}
