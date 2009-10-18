c7cb6f6c55bb5c5e96f0169f2256a6223f7ecf5f
c9s <cornelius.howl@gmail.com>
2009-10-18 16:54:55 +0800

    let PPI run faster: just take source from head -n20

--- utils/find_base_classes.pl
+++ utils/find_base_classes.pl
@@ -11,7 +11,9 @@ use constant depth => 3;
 
 sub find_base_classes {
     my $file  = shift;
-    my $d = PPI::Document->new( $file );
+    my $head = qx(head -n 20 $file);
+    print $head;
+    my $d = PPI::Document->new( \$head );
     my $sts = $d->find( sub {
         return $_[1]->isa('PPI::Statement::Include') and $_[1]->type eq 'use'
     });
