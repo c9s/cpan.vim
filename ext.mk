ac097291121598180e5f491c6d43ac4f46299ef8
c9s <cornelius.howl@gmail.com>
2010-03-30 00:21:44 +0800

    Fix ext.mk

--- ext.mk
+++ ext.mk
@@ -4,13 +4,15 @@
 INSTALL_CPANM_TO=~/bin/cpanm
 
 install-cpanm-git:
-	git clone git://github.com/miyagawa/cpanminus.git
-	cd cpanminus.git
-	perl Makefile.PL && make && sudo make install
+	cd /tmp ; \
+		git clone git://github.com/miyagawa/cpanminus.git ; \
+			cd cpanminus ;  \
+				perl Makefile.PL && make && sudo make install
 
 install-cpanm:
 	mkdir -p ~/bin/
-	if [[ -n `which wget` ]] ; then wget http://xrl.us/cpanm -O $(INSTALL_CPANM_TO) ; \
+	if [[ -n `which wget` ]] ; then wget -c http://xrl.us/cpanm -O $(INSTALL_CPANM_TO) ; \
 	elif [[ -n `which curl` ]] ; then curl http://xrl.us/cpanm -o $(INSTALL_CPANM_TO) ; \
 	fi
-	chmod +x $(INSTALL_CPANM_TO)
+	@chmod +x $(INSTALL_CPANM_TO)
+	@echo "Please add ~/bin to your PATH env variable"
